a4c901e41e36e87ade4f62abedd699eb5aad48a0
alvi2496 <alvi.utsab@gmail.com>
2018-09-28 02:32:10 -0700

    complete naive bayes text classifier

--- 1st_Term/Data_Mining/codes/decision_trees/main.py
+++ 1st_Term/Data_Mining/codes/decision_trees/main.py
@@ -2,6 +2,6 @@ import entropy
 import average_entropy
 import information_gain
 
-print("Entropy = " + str(entropy.calculate([2,3])))
-print("Average Entropy = " + str(average_entropy.calculate([[2,3],[4,0],[3,2]])))			
+print("Entropy = " + str(entropy.calculate([8,3,1])))
+print("Average Entropy = " + str(average_entropy.calculate([[7,2,3],[8,3,1]])))			
 print("Information gain = " + str(information_gain.calculate([[2,3],[4,0],[3,2]])))
--- 1st_Term/Data_Mining/codes/naive_bayes_text_classifier/main.py
+++ 1st_Term/Data_Mining/codes/naive_bayes_text_classifier/main.py
@@ -7,5 +7,92 @@ traindata = pd.concat([traintexts, trainlabels], axis=1)
 testtexts = pd.read_csv("datasets/testdata.txt", sep="\n", header=None, names=['text'])
 testlabels = pd.read_csv("datasets/testlabels.txt", sep="\n", header=None, names=['value'])
 testdata = pd.concat([testtexts, testlabels], axis=1)
-print(traindata.head())
-print(testdata.head())
+
+total_document_length = len(traindata.axes[0])
+result = set()
+traindata.text.str.lower().str.split().apply(result.update)
+total_distinct_words = len(result)
+
+p_c = len(traindata.query("value == '1'").axes[0])/total_document_length
+p_not_c = len(traindata.query("value == '0'").axes[0])/total_document_length
+
+total_words_in_c = traindata.query("value == '1'").text.str.split().apply(len).sum()
+total_words_in_not_c = traindata.query("value == '0'").text.str.split().apply(len).sum()
+
+
+
+# Evaluate with traindata
+f = open("datasets/resultlabels_traindata.txt","w+")
+for text in traintexts.text:
+	input_array = text.split()
+
+	p_fp = p_c
+	p_ws = p_not_c
+
+	# For 1
+	for w in input_array:
+		p_fp *= ( (traindata.query("value == '1'").text.str.count(w).sum() + 1) / 
+			(total_words_in_c + total_distinct_words) )
+
+	# for 0
+	for w in input_array:
+		p_ws *= ( (traindata.query("value == '0'").text.str.count(w).sum() + 1) / 
+			(total_words_in_not_c + total_distinct_words) )
+
+	if( p_fp >= p_ws ):
+		f.write("1\n")
+	elif( p_ws > p_fp ):
+		f.write("0\n")		
+
+f.close()
+
+resultlabels = pd.read_csv("datasets/resultlabels_traindata.txt", 
+	sep="\n", header=None, names=['value'])
+
+wrong_counter = 0
+for i in range(len(trainlabels.axes[0])):
+	if( trainlabels.value[i] != resultlabels.value[i] ):
+		wrong_counter += 1
+
+correctness = round( 1 - ( wrong_counter / len( trainlabels.axes[0] ) ), 4 ) * 100
+r = open("result.txt","a")
+r.write("Achieved "+str(correctness)+"%"+" correctness evaluating with traindata\n")
+r.close()
+
+# Evaluate with testdata
+f = open("datasets/resultlabels_testdata.txt","w+")
+for text in testtexts.text:
+	input_array = text.split()
+
+	p_fp = p_c
+	p_ws = p_not_c
+
+	# For 1
+	for w in input_array:
+		p_fp *= ( (traindata.query("value == '1'").text.str.count(w).sum() + 1) / 
+			(total_words_in_c + total_distinct_words) )
+
+	# for 0
+	for w in input_array:
+		p_ws *= ( (traindata.query("value == '0'").text.str.count(w).sum() + 1) / 
+			(total_words_in_not_c + total_distinct_words) )
+
+	if( p_fp >= p_ws ):
+		f.write("1\n")
+	elif( p_ws > p_fp ):
+		f.write("0\n")		
+
+f.close()
+
+resultlabels = pd.read_csv("datasets/resultlabels_testdata.txt", 
+	sep="\n", header=None, names=['value'])
+
+wrong_counter = 0
+for i in range(len(testlabels.axes[0])):
+	if( testlabels.value[i] != resultlabels.value[i] ):
+		wrong_counter += 1
+
+correctness = round( 1 - ( wrong_counter / len( testlabels.axes[0] ) ), 4 ) * 100
+r = open("result.txt","a")
+r.write("Achieved "+str(correctness)+"%"+" correctness evaluating with testdata\n\n")
+r.close()	
